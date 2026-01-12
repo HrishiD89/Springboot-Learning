1b25fa9eeac8b5ed7bc65ac5f575cf8718bb1c47
hrishikesh kalita <hrishi282k@gmail.com>
2026-01-12 23:26:08 +0530

    SpringMVC tutorial

---
+++ 02-rest-api/springMVCtutorial/src/main/java/com/app/springmvctutorial/SpringMvCtutorialApplication.java
@@ -0,0 +1,13 @@
+package com.app.springmvctutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvCtutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvCtutorialApplication.class, args);
+    }
+
+}
---
+++ 02-rest-api/springMVCtutorial/src/test/java/com/app/springmvctutorial/SpringMvCtutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.springmvctutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvCtutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
